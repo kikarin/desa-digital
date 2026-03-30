1040dd4a515cd8281f89a27d3902333a85bcb010
kikarin <akunamazon4811@gmail.com>
2026-03-30 14:39:07 +0700

    update superadmin

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -24,7 +24,7 @@ class UsersSeeder extends Seeder
         $listData = array_merge($listData, [
             [
                 'name'                 => 'Imran',
-                'email'                => 'superadmin@gmail.com',
+                'email'                => 'digitaldesagaluga@gmail.com',
                 'email_verified_at'    => null,
                 'tanggal_lahir'        => null,
                 'no_hp'                => '08512312311',
